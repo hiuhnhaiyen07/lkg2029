0affd9bd9c4b15464857c1fd7c6732f2796f209d
hiuhnhaiyen07 <hiuhnhaiyen27@gmail.com>
2026-01-28 22:15:16 +0700

    Add secure.js for dynamic URL generation

---
+++ assets/secure.js
@@ -0,0 +1,14 @@
+(function () {
+  const a = "https://hiuhnhaiyen07.github.io/dnsdakybycda/";
+  const b = "locket";
+  const c = "gold.mobile";
+  const d = "config";
+  window.__DL__ = a + b + c + d;
+
+  const m1 = "https://raw.githubusercontent.com/";
+  const m2 = "PhucAnhIT/ALL-IN-ONE/";
+  const m3 = "refs/heads/main/";
+  const m4 = "ALL_IN_ONE.sgmodule";
+  const m5 = "?v123";
+  window.__MODULE__ = m1 + m2 + m3 + m4 + m5;
+})();
